cbf92fbda72aebf68ea745e8214802cbb090cda1
mika631 <kikijiji336@gmail.com>
2019-11-11 13:27:12 +0900

    make route(words)

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,7 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   root to: 'words#index'
-  resources :words, only:[:index, :new]
+  resources :words, only:[:index, :new, :create]
   # get 'words' => 'words#index'
   # get 'words/new' -> 'words#new'
 end
