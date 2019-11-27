dfb20d817201886fab98c0c393f9c8286016abac
mika631 <kikijiji336@gmail.com>
2019-11-28 08:28:48 +0900

    update index_view(random)

--- app/controllers/words_controller.rb
+++ app/controllers/words_controller.rb
@@ -1,4 +1,16 @@
 class WordsController < ApplicationController
   def index
+    @words = Word.all
+    @random = Word.order("Rand()").limit(2)
   end
+
+  def new
+    @word = Word.new
+  end
+
+  def show
+    @word = Word.find(params[:id])
+    @wordcreater = Wordcreater.find_by(id: @word.wordcreater.id)
+  end
+
 end
