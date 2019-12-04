1ba3ebb1b46b7eb8ee731b4a85faf880fe4dec8c
mika631 <kikijiji336@gmail.com>
2019-12-04 17:03:25 +0900

    Update routes.rb

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,6 @@
 Rails.application.routes.draw do
   devise_for :users
+
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   root to: 'words#index'
   resources :words, only:[:index, :show]
@@ -7,4 +8,6 @@ Rails.application.routes.draw do
   resources :wordcreaters, only:[:new, :create]
   # get 'words' => 'words#index'
   # get 'words/new' -> 'words#new'
+
+  get 'users/:id' => 'users#show'
 end
