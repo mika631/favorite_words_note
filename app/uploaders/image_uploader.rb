f418b125686d0f15dd9edc985e51f922475e44f5
mika631 <kikijiji336@gmail.com>
2019-12-09 12:14:17 +0900

    update　carriewave,imageuplpader
    開発環境とデプロイ（heroku)での画像保存先を条件分岐

--- app/uploaders/image_uploader.rb
+++ app/uploaders/image_uploader.rb
@@ -6,8 +6,15 @@ class ImageUploader < CarrierWave::Uploader::Base
 
   # Choose what kind of storage to use for this uploader:
   # storage :file
-  storage :fog
+  # storage :fog
   # process :convert => 'jpg'
+
+  if Rails.env.production?
+    storage :fog
+  else
+    storage :file
+  end
+
 
   # Override the directory where uploaded files will be stored.
   # This is a sensible default for uploaders that are meant to be mounted:
--- config/initializers/carrierwave.rb
+++ config/initializers/carrierwave.rb
@@ -1,39 +1,15 @@
-require 'carrierwave/storage/abstract'
-require 'carrierwave/storage/file'
-require 'carrierwave/storage/fog'
+# require 'carrierwave/storage/abstract'
+# require 'carrierwave/storage/file'
+# require 'carrierwave/storage/fog'
 
-CarrierWave.configure do |config|
-  if Rails.env.development? || Rails.env.test? #開発とテストは今まで通りに
-    config.storage = :file
-  elsif Rails.env.production? #本番はS3に保存する
-    config.storage = :fog
-    config.fog_provider = 'fog/aws'
+if Rails.env.production?
+  CarrierWave.configure do |config|
     config.fog_credentials = {
       provider: 'AWS',
-      aws_access_key_id: Rails.application.credentials.aws[:access_key_id],
-      aws_secret_access_key: Rails.application.credentials.aws[:secret_access_key],
-# credentials下にaws_access_key_idとaws_secret_access_keyはあるよ
+      aws_access_key_id: ENV['ACCESS_KEY_ID'],
+      aws_secret_access_key: ENV['SECRET_ACCESS_KEY'],
       region: 'ap-northeast-1'
     }
-    config.fog_directory  = 'miiiika'
-    config.asset_host = 'https://s3-ap-northeast-1.amazonaws.com/miiika'
+    config.fog_directory = 'miiiika'
   end
 end
-
-
-
-# if Rails.env.production?
-# CarrierWave.configure do |config|
-#   config.cache_dir = "#{Rails.root}/tmp/uploads"
-#   config.fog_provider = 'fog/aws'
-#   config.fog_credentials = {
-
-#     provider: 'AWS',
-#     region:  ENV['S3_REGION'],
-#     aws_access_key_id: ENV['AWS_ACCESS_KEY_ID'],
-#     aws_secret_access_key: ENV['AWS_SECRET_ACCESS_KEY'],
-
-#   }
-#   config.fog_directory  = ENV['S3_BUCKET']
-# end
-# end
