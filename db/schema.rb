52beefb2222c3d40f5ef18b548738e02cfb37ae8
mika631 <kikijiji336@gmail.com>
2019-12-25 13:02:26 +0900

    Update schema.rb

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_12_02_070839) do
+ActiveRecord::Schema.define(version: 2019_12_22_064050) do
 
   create_table "comments", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
     t.text "comment"
@@ -36,6 +36,7 @@ ActiveRecord::Schema.define(version: 2019_12_02_070839) do
     t.string "name"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.integer "user_id"
   end
 
   create_table "words", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
